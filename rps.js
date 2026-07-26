7cdcf1decfaa1a520fadbedc92df604556d996cb
Ben Fullenkamp <168487288+bfull8@users.noreply.github.com>
2026-07-25 23:35:23 -0400

    created playGame() to play 5 rounds and compare results

--- rps.js
+++ rps.js
@@ -20,9 +20,6 @@ function getHumanChoice() {
   return prompt("Enter Rock, Paper, or Scissors:");
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
 // Create a function called playRound(humanChoice, computerChoice)
 // Convert human choice to lowercase (matches computerChoice)
 // Compare choices to determine the winner
@@ -37,21 +34,52 @@ function playRound(humanChoice, computerChoice) {
     (lowerHumanChoice == "paper" && computerChoice == "rock") ||
     (lowerHumanChoice == "scissors" && computerChoice == "paper")
   ) {
-    humanScore++;
     console.log(`You win! ${lowerHumanChoice} beats ${computerChoice}`);
+    return 1;
   } else if (
     (lowerHumanChoice == "rock" && computerChoice == "rock") ||
     (lowerHumanChoice == "paper" && computerChoice == "paper") ||
     (lowerHumanChoice == "scissors" && computerChoice == "scissors")
   ) {
     console.log(`It's a tie! You both chose ${computerChoice}`);
+    return 2;
   } else {
-    computerScore++;
     console.log(`You lose! ${computerChoice} beats ${lowerHumanChoice}`);
+    return 3;
   }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+// Create a function called playGame();
+// Move global variables to playGame()
+// Refactor play round to return true or false to increment scores
+// Create a for loop to play the round 5 times, asking for a new selection each time
+// Compare the scores at the end to determine the winner
 
-playRound(humanSelection, computerSelection);
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    const gameResult = playRound(humanSelection, computerSelection);
+
+    switch (gameResult) {
+      case 1:
+        humanScore++;
+        break;
+      case 3:
+        computerScore++;
+        break;
+    }
+  }
+
+  if (humanScore > computerScore) {
+    console.log(`You won with a score of ${humanScore} to ${computerScore}`);
+  } else if (humanScore < computerScore) {
+    console.log(`You lost with a score of ${humanScore} to ${computerScore}`);
+  } else {
+    console.log(`You tied with a score of ${humanScore} to ${computerScore}`);
+  }
+}
+
+playGame();
